b17c8c7a4fe03f729b5d7fe4beb6bc6f9401d43b
Melissa Samworth <melissasamworth@gmail.com>
2026-03-22 17:11:16 -0400

    About us final copy
    Changed all copy on about page, and in footer, to reflect what Bryce and Melissa agreed on.
    
    Small CSS change on about page to what Melissa and Bryce agreed on.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,10 +13,12 @@ export default function AboutPage() {
       <h1 className="padding-top-56px padding-bottom-40px">About us</h1>
 
       <h2 className="width-9-col-narrow padding-bottom-56px">
-        We aim to provide a{' '}
-        <span className="color-light-teal">central and comprehensive hub</span>{' '}
-        for AI safety, through which individuals can easily discover the most
-        impactful resources for them.
+        We&apos;re a small nonprofit driven by 1.25 salaried employees and lots
+        of volunteers. We aim to multiply global AI safety efforts through a{' '}
+        <span className="color-light-teal">
+          centralized, comprehensive, and up-to-date resource hub
+        </span>
+        .
       </h2>
 
       <div className="flex gap-56px flex-col-mobile">
@@ -25,23 +27,23 @@ export default function AboutPage() {
           <div>
             <h3 className="padding-bottom-16px">Funding</h3>
             <p className="color-teal-300 padding-bottom-40px">
-              We are operating on $100k of funding from the Survival and
-              Flourishing Fund (SFF), a grantmaker that funds lorem ipsum dolor
-              sit amet kind of projects. This pays for 1 full-time, and 1
-              part-time salary (everyone else is volunteer) plus some
-              miscellaneous costs, like website hosting.
+              This project operates on about $100k USD of annual funding from
+              the Survival and Flourishing Fund, a grantmaker that supports
+              projects working on the long-term survival and flourishing of
+              sentient life. This pays for 1 full-time and 1 part-time salary
+              (everyone else is a volunteer) plus some other costs, like website
+              hosting.
             </p>
 
             <h3 className="padding-bottom-16px">
-              Financial motivations (or lack thereof)
+              (Lack of) financial motivations
             </h3>
             <p className="color-teal-300">
-              The two team members that receive compensation have never made a
-              decision based on whether it would land us more funding, nor have
-              we been influenced or pressured by SFF. We genuinely care about
-              this topic, and making our decisions based on what most helps the
-              world seems to be totally adequate for receiving our modest amount
-              of funding.
+              We work on this project because we care about AI safety. The two
+              team members that receive compensation make project decisions
+              based solely on what they believe will have the greatest impact.
+              This approach has proven to be totally aligned with our funders’
+              goals.
             </p>
           </div>
         </div>
@@ -50,23 +52,13 @@ export default function AboutPage() {
         <div className={`${styles.questionsCard} width-5-col-narrow`}>
           <p className="color-white paragraph-default-bold">Questions?</p>
           <p className="color-teal-300 padding-top-16px paragraph-small">
-            We&apos;re just a few &quot;normal&quot; people, and we really want
-            to be transparent and accessible. Find our contact info below!
+            Takes? Suggestions? Requests?
           </p>
-          <a href="#team" className={styles.arrowCircle}>
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5.05316 11.5468C4.8579 11.3515 4.54132 11.3515 4.34606 11.5468C4.15079 11.742 4.15079 12.0586 4.34606 12.2539L7.54186 15.4497C7.63265 15.5426 7.75937 15.6003 7.89956 15.6003C8.01138 15.6003 8.11462 15.5636 8.1979 15.5016C8.21734 15.4872 8.23582 15.4712 8.25316 15.4539L11.4532 12.2539C11.6484 12.0586 11.6484 11.742 11.4532 11.5468C11.2579 11.3515 10.9413 11.3515 10.7461 11.5468L8.39956 13.8933L8.39956 0.90039C8.39956 0.624248 8.1757 0.40039 7.89956 0.40039C7.62342 0.40039 7.39956 0.624248 7.39956 0.90039L7.39956 13.8932L5.05316 11.5468Z"
-                fill="#094141"
-              />
-            </svg>
-          </a>
+          <div className="padding-top-24px">
+            <a href="#team" className="button-secondary">
+              Send us an email
+            </a>
+          </div>
         </div>
       </div>
 
@@ -75,8 +67,7 @@ export default function AboutPage() {
         id="team"
         className="padding-top-80px padding-bottom-56px width-7-col-narrow"
       >
-        We are built and sustained on <em>lots</em> of volunteer effort, plus
-        1.25 salaried employees.
+        We consider AISafety.com a community project, led by a few core members.
       </h2>
 
       <div className="flex flex-col gap-56px">
@@ -102,10 +93,9 @@ export default function AboutPage() {
             <p className="paragraph-small color-teal-300 padding-bottom-16px">
               Søren read the Sequences in 2014 and set out to verify whether AI
               risk was real. He founded AI Safety Danmark in 2016, bought the
-              AISafety.com domain in 2017 (he&apos;s had some major bidders
-              since, and always turns them down), and runs a reading group now
-              past 337 sessions. As a father of two, protecting his family is
-              his greatest motivation.
+              AISafety.com domain the next year, and runs an AI safety reading
+              group now past 300 meetings. As a father of two, protecting his
+              family is his greatest motivation.
             </p>
             <a
               href="mailto:soeren@aisafety.com"
@@ -141,9 +131,11 @@ export default function AboutPage() {
               </div>
             </div>
             <p className="paragraph-small color-teal-300 padding-bottom-16px">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation.
+              Bryce was blown away by GPT-4&apos;s capabilities when it was
+              released in 2023, but quickly discovered that alongside its
+              enormous potential, AI also poses an existential risk. He closed
+              his video agency and pivoted his career to working full time on AI
+              safety.
             </p>
             <a
               href="mailto:bryce@aisafety.com"
@@ -174,10 +166,11 @@ export default function AboutPage() {
               </div>
             </div>
             <p className="paragraph-small color-teal-300">
-              Melissa found AI safety through Effective Altruism and Animal
-              Welfare. She is inspired by the good AI could do to advance human
-              and animal flourishing, if it can be developed with extreme
-              responsibility.
+              Melissa got into AI safety in 2023 when she realized aligned
+              superintelligence served the best shot at fulfilling the
+              Buddhist-inspired mantra ‘may all sentient beings be free of
+              suffering’. But future AI could go unimaginably well or
+              unimaginably badly. She set out to help things go well.
             </p>
           </div>
 
@@ -204,9 +197,9 @@ export default function AboutPage() {
 
       {/* Volunteers & community section */}
       <div className="flex gap-56px padding-top-104px flex-col-mobile">
-        <h2 className="width-4-col-narrow">
+        <h3 className="width-4-col-narrow">
           30+ volunteers &amp; tons of takes from the community (thank you!)
-        </h2>
+        </h3>
 
         <div className="flex flex-col gap-56px width-4-col-narrow">
           <div>
@@ -236,8 +229,9 @@ export default function AboutPage() {
               />
             </div>
             <p className="paragraph-small color-teal-300 padding-top-24px padding-bottom-24px">
-              We do our best to structure our takes and decisions based on the
-              wisdom of the community at large.
+              We built this site for the AI safety community – and we&apos;re
+              always looking to make it more useful. If you&apos;ve got
+              thoughts, we&apos;re listening.
             </p>
             <a
               href="https://discord.gg/faamWzPcv8"
@@ -253,8 +247,9 @@ export default function AboutPage() {
             <Image src="/images/add-doc.svg" alt="" width={16} height={16} />
           </div>
           <p className="paragraph-small color-teal-300 padding-top-24px padding-bottom-24px">
-            We consider AISafety.com a community project, and encourage
-            community members to add listings that we may have missed.
+            We encourage community members to add listings we may have missed
+            through the ‘Suggest listing’ button on each listing page, or
+            suggest corrections using the button below.
           </p>
           <a
             href="https://airtable.com/appF8XfZUGXtfi40E/pagndDvdya1DSqoxN/form"
